5cf13eb1b284c51f25eb4b79ea4a69f74b8dee07
AgaboMonteiro <94582215+AgaboMonteiro@users.noreply.github.com>
2025-09-07 23:25:38 -0300

    Add files via upload

---
+++ Atividade/Atividade11.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Atividade11
+{
+    static string decodificar(string fraseCodificada)
+    {
+        int n = fraseCodificada.Length;
+        int pos = 0;
+        char[] decodifica = new char[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            if (fraseCodificada[i] == 'p' && i + 1 < n && fraseCodificada[i + 1] != ' ')
+            {
+                
+                decodifica[pos] = fraseCodificada[i + 1];
+                pos++;
+                i++; 
+            }
+            else if (fraseCodificada[i] != 'p')
+            {
+                decodifica[pos] = fraseCodificada[i];
+                pos++;
+            }
+        }
+
+        // Cria a string só com os caracteres válidos
+        return new string(decodifica, 0, pos);
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("Entre com uma frase codificada:");
+        string frase = Console.ReadLine();
+        Console.WriteLine("Frase decodificada: " + decodificar(frase));
+    }
+}
