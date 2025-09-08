fd0c6ab2834098f8a466558466980ea951013edf
AgaboMonteiro <94582215+AgaboMonteiro@users.noreply.github.com>
2025-09-07 23:10:58 -0300

    Add files via upload

---
+++ Atividade11.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Atividade11
+{
+    static void Main()
+    {
+        string mensagem = Console.ReadLine();
+        string resultado = "";
+
+        for (int i = 0; i < mensagem.Length; i++)
+        {
+            if (mensagem[i] == 'p' && i + 1 < mensagem.Length && mensagem[i + 1] != ' ')
+            {
+                resultado = resultado + mensagem[i + 1];
+                i++;
+            }
+            else if (mensagem[i] != 'p')
+            {
+                resultado = resultado + mensagem[i];
+            }
+        }
+
+        Console.WriteLine(resultado);
+    }
+}
